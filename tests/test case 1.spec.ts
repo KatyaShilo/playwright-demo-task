f457980e732550b413d9d1e5d14186ad1da92ab4
Shilo <ekaterina.shilo@sogeti.com>
2024-02-07 23:59:54 +0100

    Add Locators for Test Case 2

--- tests/test case 1.spec.ts
+++ tests/test case 1.spec.ts
@@ -17,6 +17,27 @@ describe("Automation Task",() => {
     await expect(automationLocator).toHaveClass(/selected/);
     await page.close();  
   });
+
+
+  test('Test Case 2', async ({ page }) => {
+    const servicesLocator = page.locator('#main-menu > ul > li:nth-child(3)');
+    const automationLocator = page.locator('div [data-levelname="level2"] > ul > li:nth-child(4)');
+    const contactUsLocator = page.getByText('Contact us:');
+    const firstNameLocator = page.getByText('First Name');
+    const lastNameLocator = page.getByText('Last Name');
+    const emailLocator = page.getByText('Email');
+    const phoneLocator = page.getByText('Phone');
+    const messageLocator = page.getByText('Message');
+
+    await page.goto('https://www.sogeti.com/');
+    await page.getByRole('button', { name: 'Allow all cookies' }).click();
+    await servicesLocator.click();
+    await automationLocator.click();
+    await expect(contactUsLocator).toBeVisible();
+    await firstNameLocator.fill("Tester")
+    
+    await page.close();  
+  });
 });
 
 
