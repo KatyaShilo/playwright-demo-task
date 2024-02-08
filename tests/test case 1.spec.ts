db9de709dd0fde7b753ca2b2fb192496962bfe42
Katya Shilo <yakaterinashilo@gmail.com>
2024-02-08 21:05:48 +0100

    modified Test Case 1

--- tests/test case 1.spec.ts
+++ tests/test case 1.spec.ts
@@ -6,9 +6,10 @@ describe("Automation Task",() => {
   test('Test Case 1', async ({ page }) => {
     const servicesLocator = page.locator('#main-menu > ul > li:nth-child(3)');
     const automationLocator = page.locator('div [data-levelname="level2"] > ul > li:nth-child(4)');
+    const allowCookiesButton = page.getByRole('button', { name: 'Allow all cookies' });
   
     await page.goto('https://www.sogeti.com/');
-    await page.getByRole('button', { name: 'Allow all cookies' }).click();
+    await allowCookiesButton.click();
     await servicesLocator.click();
     await automationLocator.click();
     await expect(page.locator('h1')).toContainText('Automation');
@@ -17,27 +18,5 @@ describe("Automation Task",() => {
     await expect(automationLocator).toHaveClass(/selected/);
     await page.close();  
   });
-
-
-  test('Test Case 2', async ({ page }) => {
-    const servicesLocator = page.locator('#main-menu > ul > li:nth-child(3)');
-    const automationLocator = page.locator('div [data-levelname="level2"] > ul > li:nth-child(4)');
-    const contactUsLocator = page.getByText('Contact us:');
-    const firstNameLocator = page.getByText('First Name');
-    const lastNameLocator = page.getByText('Last Name');
-    const emailLocator = page.getByText('Email');
-    const phoneLocator = page.getByText('Phone');
-    const messageLocator = page.getByText('Message');
-
-    await page.goto('https://www.sogeti.com/');
-    await page.getByRole('button', { name: 'Allow all cookies' }).click();
-    await servicesLocator.click();
-    await automationLocator.click();
-    await expect(contactUsLocator).toBeVisible();
-    await firstNameLocator.fill("Tester")
-    
-    await page.close();  
-  });
 });
-
 
