b174d93e4b57b1a9de8493dafa9bcd48acda478f
Katya Shilo <yakaterinashilo@gmail.com>
2024-02-09 15:45:28 +0100

    finished api test case 2

--- tests/api-tc-1.spec.ts
+++ tests/api-tc-1.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('API Test Case 1', async ({ request }) => {
   const recievedData = await request.get(`/de/bw/stuttgart`);
-  console.log('response', await recievedData.json())
+  
   expect(recievedData.status()).toBe(200);
   expect(recievedData.headers()['content-type']).toBe('application/json');
   expect(await recievedData.json()).toEqual(expect.objectContaining({
--- tests/api-tc-2.spec.ts
+++ tests/api-tc-2.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('API Test Case 2', async ({ request }) => {
-  const recievedData = await request.get(`/us/90210`);
-  console.log('response', await recievedData.json())
-  expect(recievedData.status()).toBe(200);
-  expect(recievedData.headers()['content-type']).toBe('application/json');
-  expect(await recievedData.json()).toEqual(expect.objectContaining({
+  const recievedData1 = await request.get(`/us/90210`);
+  const recievedData2 = await request.get(`/us/12345`);
+  const recievedData3 = await request.get(`/ca/B2R`);
+
+  expect(recievedData1.status()).toBe(200);
+  expect(recievedData1.headers()['content-type']).toBe('application/json');
+  expect(await recievedData1.json()).toEqual(expect.objectContaining({
     country: 'United States',
     'post code': '90210',
     places: [{
@@ -16,4 +18,33 @@ test('API Test Case 2', async ({ request }) => {
       latitude: '34.0901'
     }]
   }));
+
+  expect(recievedData2.status()).toBe(200);
+  expect(recievedData2.headers()['content-type']).toBe('application/json');
+  expect(await recievedData2.json()).toEqual(expect.objectContaining({
+    country: 'United States',
+    'post code': '12345',
+    places: [{
+        'place name': 'Schenectady',
+        longitude: '-74.058',
+        state: 'New York',
+        'state abbreviation': 'NY',
+        latitude: '42.8333'
+      }]
+  }));
+
+  expect(recievedData3.status()).toBe(200);
+  expect(recievedData3.headers()['content-type']).toBe('application/json');
+  expect(await recievedData3.json()).toEqual(expect.objectContaining({
+    country: 'Canada',
+    'post code': 'B2R',
+    places: [{
+      'place name': 'Waverley',
+      longitude: '-63.5144',
+      state: 'Nova Scotia',
+      'state abbreviation': 'NS',
+      latitude: '44.7431'
+      }]
+  }));
+
 });
