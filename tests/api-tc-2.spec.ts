99cf9fca32f16c4f99365d5ee9941bf1f0ebbe03
Katya Shilo <yakaterinashilo@gmail.com>
2024-02-09 14:07:36 +0100

    added api test case 2

---
+++ tests/api-tc-2.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('API Test Case 2', async ({ request }) => {
+  const recievedData = await request.get(`/us/90210`);
+  console.log('response', await recievedData.json())
+  expect(recievedData.status()).toBe(200);
+  expect(recievedData.headers()['content-type']).toBe('application/json');
+  expect(await recievedData.json()).toEqual(expect.objectContaining({
+    country: 'United States',
+    'post code': '90210',
+    places: [{
+      'place name': 'Beverly Hills',
+      longitude: '-118.4065',
+      state: 'California',
+      'state abbreviation': 'CA',
+      latitude: '34.0901'
+    }]
+  }));
+  //expect(await recievedData.json()).toContainEqual({ 'place name': 'Beverly Hills' });
+  //expect(await recievedData.json().places).objectContaining({ 'place name': 'Beverly Hills' });
+
+});
